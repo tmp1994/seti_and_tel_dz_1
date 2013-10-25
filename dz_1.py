ee22c40129bc5cf31d96f305ea8d694b029bd3ce
Vitaly <vit.kanivets@gmail.com>
2013-10-25 17:50:50 +0400

    Added cycle

--- dz_1.py
+++ dz_1.py
@@ -24,25 +24,25 @@ def check_sum(u):
 
 
 def main():
-    #for ee in range(0, 7):
-    a = [1, 1, 1, 0]  # исходное число
-    ee = 4  # в каком бите допущена ошибка [0, 6]
-    b = [0, 0, a[0], 0, a[1], a[2], a[3]]
-    check_sum(b)
+    for ee in range(0, 7):
+        a = [1, 1, 1, 0]  # исходное число
+        # ee = 4  # в каком бите допущена ошибка [0, 6]
+        b = [0, 0, a[0], 0, a[1], a[2], a[3]]
+        check_sum(b)
 
-    d = deepcopy(b)
-    d[ee] = n(d[ee])
+        d = deepcopy(b)
+        d[ee] = n(d[ee])
 
-    m = [0, 0, d[2], 0, d[4], d[5], d[6]]
-    check_sum(m)
+        m = [0, 0, d[2], 0, d[4], d[5], d[6]]
+        check_sum(m)
 
-    res = []
-    for i in range(0, 7):
-        if d[i] != m[i]:
-            res.append(i+1)
-    ww = sum(res)-1  # индекс ошибочного бита
-    d[ww] = n(d[ww])
-    r = [d[2], d[4], d[5], d[6]]  # рез-ат с исправленной ошибкой
+        res = []
+        for i in range(0, 7):
+            if d[i] != m[i]:
+                res.append(i+1)
+        ww = sum(res)-1  # индекс ошибочного бита
+        d[ww] = n(d[ww])
+        r = [d[2], d[4], d[5], d[6]]  # рез-ат с исправленной ошибкой
 
     print [p(a[i], r[i]) for i in range(0, 4)]
 
